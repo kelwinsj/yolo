0772dd47f5b46c8d22ae4a8e0c99afad3635fd83
kelwinsj <kelwinsjohn@gmail.com>
2024-05-29 22:39:41 +0300

    Added ansible roles and main terraform config file

---
+++ Stage_two/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    vagrant = {
+      source  = "bmatcuk/vagrant"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
+resource "vagrant_vm" "vagrantbox" {
+  vagrantfile_dir = "../"
+  get_ports = true
+
+  provisioner "local-exec" {
+  command = "vagrant provision"
+  working_dir = "../"
+}
+}
